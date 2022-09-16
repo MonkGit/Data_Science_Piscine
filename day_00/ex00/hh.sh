55189b47e32dae6f6035044f78cb15fd291fdfac
Scotty Laree <slaree@at-r1.msk.21-school.ru>
2022-09-16 21:28:18 +0300

    day00 python data science piscine

--- day_00/ex00/hh.sh
+++ day_00/ex00/hh.sh
@@ -3,6 +3,6 @@
 FILE_NAME="./hh.json"
 LENGTH="20"
 
-NAME="${1/ /+}"
+SEARCH_PARAMS="${1/ /+}"
 
-curl -k "https://api.hh.ru/vacancies?text=$NAME&per_page=$LENGTH" | jq > $FILE_NAME
+curl -s -k -H 'User-Agent: api-test-agent' "https://api.hh.ru/vacancies?text=$SEARCH_PARAMS&per_page=$LENGTH" | jq > $FILE_NAME
